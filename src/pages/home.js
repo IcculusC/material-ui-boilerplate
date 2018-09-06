784e8264a0085561bcc61087a4f6349368ae7399
Leath Cooper <leathcooper@gmail.com>
2018-09-05 22:44:49 -0700

    initial commit, material-ui demo

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,8 @@
 // @flow
 import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import Link from 'react-router-dom/Link';
 
-const styles = theme => ({
+const styles = {
   center: {
     fontFamily: 'HelveticaNeue-Light, Arial',
     display: 'flex',
@@ -11,52 +10,53 @@ const styles = theme => ({
     justifyContent: 'center',
     height: '100%',
   },
+  fullHeightDiv: {
+    height: '100%',
+    backgroundColor: '#FFFFFF',
+  },
+  fusionStyle: {
+    fontSize: '80px',
+    color: 'rgba(0,0,0,.8)',
+    paddingRight: '30px',
+    display: 'flex',
+  },
   circle: {
     height: '180px',
     width: '180px',
     marginTop: '20px',
     backgroundColor: 'white',
-    '&:hover': {backgroundColor: theme.palette.primary.light}, // {backgroundColor: '#f0f8fa'},
-    border: `10px solid ${theme.palette.primary.dark}`, // '10px solid #4db5d9',
+    '&:hover': {backgroundColor: '#f0f8fa'},
+    border: '10px solid #4db5d9',
     borderRadius: '50%',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  fullHeight: {
-    height: '100%',
-    backgroundColor: '#FFFFFF',    
-  },
-  gettingStarted: {
+  gettingStartedLink: {
     textDecoration: 'none',
-    color: theme.palette.primary.dark,
+    color: '#4db5d9',
     fontSize: '18px',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'center',
     height: '100%',
-  },
-  splashText: {
-    fontSize: '80px',
-    color: 'rgba(0,0,0,.8)',
-    paddingRight: '30px',
-    display: 'flex',
-  },
-});
+  }
+};
 
-export const Home = ({classes}) => (
-  <div className={classes.fullHeight} >
+const Home = ({classes}) => (
+  <div className={classes.fullHeightDiv} >
     <div className={classes.center} >
-      <span className={classes.splashText}>Fusion.js</span>
+      <div className={classes.fusionStyle}>Fusion.js</div>
+
       <div className={classes.center} >
         <div className={classes.circle} >
-          <Link
-            className={classes.gettingStarted}
-            to='/'
+          <a
+            className={classes.gettingStartedLink}
+            href="https://fusionjs.com/docs/getting-started"
           >
             Let&apos;s Get Started
-          </Link>
+          </a>
         </div>
       </div>
     </div>
--- src/root.js
+++ src/root.js
@@ -1,15 +1,21 @@
 // @flow
 import React from 'react';
 import {Route, Switch} from 'fusion-plugin-react-router';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
+import DemoCssBaseline from './common/DemoCssBaseline';
 import Home from './pages/home.js';
 import PageNotFound from './pages/pageNotFound.js';
 
 const root = (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route component={PageNotFound} />
-  </Switch>
+  <React.Fragment>
+    <CssBaseline />
+    <DemoCssBaseline />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route component={PageNotFound} />
+    </Switch>
+  </React.Fragment>
 );
 
 export default root;
